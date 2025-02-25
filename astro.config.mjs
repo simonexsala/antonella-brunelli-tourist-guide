1833a7658617c7a5a31e3171f81500031e04c3de
Simone Sala <39414823+simonexsala@users.noreply.github.com>
2025-02-25 11:26:29 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.antonellatours.it/',
+  // site: 'https://www.antonellatours.it/',
+  site: 'https://astro-blog-template.netlify.app',
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
